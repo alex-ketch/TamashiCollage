7a76af0c8fac30c0c6ac000a7d161488621be53f
alex-ketch <alex@ketch.me>
2016-02-20 13:57:59 -0500

    Close path if distance is less than 15

--- src/js/app/_cursor.js
+++ src/js/app/_cursor.js
@@ -1,8 +1,17 @@
 var myPath = new Path();
 myPath.strokeColor = 'black';
+myPath.fillColor = 'blue';
 
 function onMouseDown(event) {
   console.log(event.point);
-  myPath.add(event.point);
+  if (myPath._segments.length === 0) {
+    myPath.add(event.point);
+  } else {
+    var pointDist = event.point.getDistance(myPath._segments[0].point);
+    if (pointDist < 15) {
+      myPath.closed = true;
+    } else {
+      myPath.add(event.point);
+    }
+  }
 }
-
