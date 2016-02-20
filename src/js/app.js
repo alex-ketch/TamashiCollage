17299788df192a71cf742b6b4237a143d8e225cc
alex-ketch <alex@ketch.me>
2016-02-20 18:52:04 -0500

    Add rudimentary layer picking

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,33 @@ var tempPath;
 
 window._app = {
     tool: "vector",
-    activeLayer: "bg",
+    activeLayer: "layerBg",
     activePath: tempPath,
     fillColor: "#FFAABB",
-    strokeColor: "#43597D"
+    strokeColor: "#43597D",
+    layers: {
+      layerBg: {
+        fillColor: "FFAABB",
+        strokeColor: "#43597D"
+      },
+      layer1: {
+        fillColor: "red",
+        strokeColor: "#43597D"
+      },
+      layer2: {
+        fillColor: "green",
+        strokeColor: "#43597D"
+      },
+      layer3: {
+        fillColor: "blue",
+        strokeColor: "#43597D"
+      }
+    }
 };
 
 function newShape() {
   currentPath = new Path();
-  currentPath.fillColor = app.fillColor;
+  currentPath.fillColor = app.layers[app.activeLayer].fillColor;
   currentPath.strokeColor = app.strokeColor;
   currentPath.selected = true;
 }
---
+++ src/js/app/_layers.js
@@ -0,0 +1,36 @@
+var numOfLayers = document.querySelectorAll(".layers > div");
+
+function addListner(event) {
+  selectLayer(layerName);
+}
+
+function selectLayer(layer) {
+  switch (this.className) {
+    case "layerBg":
+      app.activeLayer = this.className;
+      closePath();
+      break;
+    case "layer1":
+      app.activeLayer = this.className;
+      closePath();
+      break;
+    case "layer2":
+      app.activeLayer = this.className;
+      closePath();
+      break;
+    case "layer3":
+      app.activeLayer = this.className;
+      closePath();
+      break;
+    default:
+      alert("error selecting layer!");
+  }
+}
+
+for (var i = 0; i < numOfLayers.length; i++) {
+  var layerName = numOfLayers[i].className.toString();
+
+  // layerName = new Layer();
+
+  numOfLayers[i].addEventListener('click', selectLayer);
+}
