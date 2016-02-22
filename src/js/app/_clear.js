5ef2757e31c7fb997792dfd342fc9f1f1f8b58e6
kmist <hi.kmist@gmail.com>
2016-02-21 19:39:49 -0500

    Active shortcut keys for 1, 2, 3, 0, q, w, e, p, L, N

--- src/js/app/_clear.js
+++ src/js/app/_clear.js
@@ -4,12 +4,18 @@ function clearLayer(layer) {
 }
 
 $('.clear').on('click', function(){
+  var clearConfirm = confirm("Reset layer?");
+  if (clearConfirm == true) {
   clearLayer(app.activeLayerIndex);
+}
 })
 
 $('.clearAll').on('click', function(){
+  var clearAllConfirm = confirm("Are you sure you want to start new?");
+  if (clearAllConfirm == true) {
   var numOfLayers = document.querySelectorAll(".layers > div");
   for (var i = 0; i < numOfLayers.length; i++) {
     clearLayer(i)
   }
+}
 })
--- src/js/app/_keyboard.js
+++ src/js/app/_keyboard.js
@@ -1,6 +1,6 @@
 function onKeyDown(event) {
   event.preventDefault();
-  // console.log(event.key);
+  console.log(event.key);
   if (event.key === "backspace") {
     if (!currentPath.isEmpty()) {
       currentPath.removeSegment(currentPath._segments.length - 1);
@@ -11,6 +11,40 @@ function onKeyDown(event) {
     // console.log(currentPath);
   } else if (event.key === "enter") {
     closePath();
+  } else if (event.key === "1") {
+    activateLayer("layer1", 1);
+  } else if (event.key === "2") {
+    activateLayer("layer2", 2);
+  } else if (event.key === "3") {
+    activateLayer("layer3", 3);
+  } else if (event.key === "0") {
+    activateLayer("layerBg", 0);
+  } else if (event.modifiers.shift && event.key === "n") {
+    var clearAllConfirm = confirm("Are you sure you want to start new?");
+
+    if (clearAllConfirm == true) {
+      var numOfLayers = document.querySelectorAll(".layers > div");
+      for (var i = 0; i < numOfLayers.length; i++) {
+        clearLayer(i)
+      }
+    }
+  } else if (event.modifiers.shift && event.key === "l") {
+    var clearConfirm = confirm("Reset layer?");
+    if (clearConfirm == true) {
+      clearLayer(app.activeLayerIndex);
+    }
+  } else if (event.key === "q") {
+    activateLayer("layer1", 1);
+    $('[data-remodal-id=modal]').remodal().open();
+  } else if (event.key === "w") {
+    activateLayer("layer2", 2);
+    $('[data-remodal-id=modal]').remodal().open();
+  } else if (event.key === "e") {
+    activateLayer("layer3", 3);
+    $('[data-remodal-id=modal]').remodal().open();
+  } else if (event.key === "p") {
+    activateLayer("layerBg", 0);
+    $('[data-remodal-id=modal]').remodal().open();
   }
 }
 
