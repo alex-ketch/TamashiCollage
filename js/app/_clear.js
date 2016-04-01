938cc0894163f4cdeb016dd819bbf1e06b67942f
alex-ketch <alex@ketch.me>
2016-03-31 23:57:59 -0400

    Auto-generated commit

--- js/app/_clear.js
+++ js/app/_clear.js
@@ -1,6 +1,19 @@
-function clearLayer(layer) {
+import $ from 'jquery';
+import paper from 'paper';
+import {app} from './index.js';
+import {selectLayer} from './_layers.js';
+
+export default function clearLayer(layer) {
   if (paper.project.layers[layer].index !== 5) {
-    paper.project.layers[layer].getItem({class: CompoundPath}).removeChildren(1);
+    let bounds = paper.project.view.viewSize;
+    let cleanLayer = new paper.CompoundPath({
+      clipMask: true,
+      children: [
+        new paper.Path.Rectangle(0,0, bounds._width, bounds._height)
+      ]
+    });
+
+    paper.project.activeLayer.getItem({class: paper.CompoundPath }).replaceWith(cleanLayer);
     paper.view.draw();
   } else {
     paper.project.layers[layer].getItem({class: paper.Raster}).set({
@@ -12,19 +25,17 @@ function clearLayer(layer) {
   }
 }
 
-$('.clear').on('click', function(){
-  var clearConfirm = confirm("Reset layer?");
-  if (clearConfirm == true) {
+export function selectThenClear(layer) {
+  selectLayer(layer);
   clearLayer(app.activeLayerIndex);
 }
-})
 
 $('.clearAll').on('click', function(){
   var clearAllConfirm = confirm("Are you sure you want to start new?");
   if (clearAllConfirm == true) {
-  var numOfLayers = document.querySelectorAll(".layers > div");
-  for (var i = 0; i < numOfLayers.length; i++) {
-    clearLayer(i)
+    var numOfLayers = document.querySelectorAll(".layers > div");
+    for (var i = 0; i < numOfLayers.length; i++) {
+      clearLayer(i)
+    }
   }
-}
 })
--- js/app/_keyboard.js
+++ js/app/_keyboard.js
@@ -1,67 +1,70 @@
-function onKeyDown(event) {
-  event.preventDefault();
-  if (event.key === "backspace") {
-    if (!currentPath.isEmpty()) {
-      currentPath.removeSegment(currentPath.lastSegment.index)
-    }
-  } else if (event.key === "space") {
-    // console.log(app);
-  } else if (event.key === "enter") {
-    closePath();
-  } else if (event.key === "1") {
-    activateLayer("layer1", 1);
-  } else if (event.key === "2") {
-    activateLayer("layer2", 2);
-  } else if (event.key === "3") {
-    activateLayer("layer3", 3);
-  } else if (event.key === "4") {
-    activateLayer("layer4", 4);
-  } else if (event.key === "0") {
-    activateLayer("layerBg", 0);
-  } else if (event.modifiers.shift && event.key === "n") {
-    var clearAllConfirm = confirm("Are you sure you want to start new?");
+import $ from 'jquery';
+import paper from 'paper';
+import {app, tool, closePath} from './index.js';
+import {activateLayer} from './_layers.js';
+import clearLayer from './_clear.js';
+import remodal from 'remodal';
 
-    if (clearAllConfirm == true) {
-      var numOfLayers = document.querySelectorAll(".layers > div");
-      for (var i = 0; i < numOfLayers.length; i++) {
-        clearLayer(i)
+export default function shortcuts() {
+  tool.onKeyDown = function(event) {
+    event.preventDefault();
+    if (event.key === "backspace") {
+      if (!currentPath.isEmpty()) {
+        currentPath.removeSegment(currentPath.lastSegment.index)
       }
+    } else if (event.key === "space") {
+      // console.log(app);
+    } else if (event.key === "enter") {
+      closePath();
+    } else if (event.key === "1") {
+      activateLayer("layer1", 1);
+    } else if (event.key === "2") {
+      activateLayer("layer2", 2);
+    } else if (event.key === "3") {
+      activateLayer("layer3", 3);
+    } else if (event.key === "4") {
+      activateLayer("layer4", 4);
+    } else if (event.key === "0") {
+      activateLayer("layerBg", 0);
+    } else if (event.modifiers.shift && event.key === "n") {
+      var clearAllConfirm = confirm("Are you sure you want to start new?");
+
+      if (clearAllConfirm == true) {
+        var numOfLayers = document.querySelectorAll(".layers > div");
+        for (var i = 0; i < numOfLayers.length; i++) {
+          clearLayer(i)
+        }
+      }
+    } else if (event.modifiers.shift && event.key === "l") {
+      var clearConfirm = confirm("Reset layer?");
+      if (clearConfirm == true) {
+        clearLayer(app.activeLayerIndex);
+      }
+    } else if (event.modifiers.shift && event.key === "e") {
+      canvasExport();
+    } else if (event.key === "q") {
+      activateLayer("layer1", 1);
+      $('[data-remodal-id=modal]').remodal().open();
+    } else if (event.key === "w") {
+      activateLayer("layer2", 2);
+      $('[data-remodal-id=modal]').remodal().open();
+    } else if (event.key === "e") {
+      activateLayer("layer3", 3);
+      $('[data-remodal-id=modal]').remodal().open();
+    } else if (event.key === "r") {
+      activateLayer("layer4", 4);
+      $('[data-remodal-id=modal]').remodal().open();
+    } else if (event.key === "p") {
+      activateLayer("layerBg", 0);
+      $('[data-remodal-id=modal]').remodal().open();
+    } else if (event.key === "c") {
+      app.tool = "free";
+      $('.lassoPoly').removeClass("active");
+      $('.lassoFree').addClass("active");
+    } else if (event.key === "v") {
+      app.tool = "poly";
+      $('.lassoFree').removeClass("active");
+      $('.lassoPoly').addClass("active");
     }
-  } else if (event.modifiers.shift && event.key === "l") {
-    var clearConfirm = confirm("Reset layer?");
-    if (clearConfirm == true) {
-      clearLayer(app.activeLayerIndex);
-    }
-  } else if (event.modifiers.shift && event.key === "e") {
-    canvasExport();
-  } else if (event.key === "q") {
-    activateLayer("layer1", 1);
-    $('[data-remodal-id=modal]').remodal().open();
-  } else if (event.key === "w") {
-    activateLayer("layer2", 2);
-    $('[data-remodal-id=modal]').remodal().open();
-  } else if (event.key === "e") {
-    activateLayer("layer3", 3);
-    $('[data-remodal-id=modal]').remodal().open();
-  } else if (event.key === "r") {
-    activateLayer("layer4", 4);
-    $('[data-remodal-id=modal]').remodal().open();
-  } else if (event.key === "p") {
-    activateLayer("layerBg", 0);
-    $('[data-remodal-id=modal]').remodal().open();
-  } else if (event.key === "c") {
-    app.tool = "free";
-    $('.lassoPoly').removeClass("active");
-    $('.lassoFree').addClass("active");
-  } else if (event.key === "v") {
-    app.tool = "poly";
-    $('.lassoFree').removeClass("active");
-    $('.lassoPoly').addClass("active");
   }
 }
-
-$(".shortcut").attr("data-remodal-target", "modalKeys");
-$( document ).ready(function() {
-  $('[data-remodal-id=modalKeys]').remodal().open();
-  $('.lassoFree').addClass("active");
-});
