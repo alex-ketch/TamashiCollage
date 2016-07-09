8514757c4f68ccce0663843775edb6770e3c55b2
alex-ketch <alex@ketch.me>
2016-07-09 19:41:56 -0400

    Update function calls to match new structure

--- src/js/app/_keyboard.js
+++ src/js/app/_keyboard.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import paper from 'paper';
 import {closePath} from './_shapes.js';
 import {app, tool} from './_settings.js';
-import {activateLayer, selectLayer} from './_layers.js';
+import {selectLayer} from './_layers.js';
 import clearLayer from './_clear.js';
 import remodal from 'remodal';
 
@@ -10,55 +10,54 @@ export default function shortcuts() {
   tool.onKeyDown = function(event) {
     event.preventDefault();
     if (event.key === "backspace") {
-      if (!currentPath.isEmpty()) {
-        currentPath.removeSegment(currentPath.lastSegment.index);
+      if (app.currentPath && !app.currentPath.isEmpty()) {
+        app.currentPath.removeSegment(app.currentPath.lastSegment.index);
       }
     } else if (event.key === "space") {
-      // console.log(app);
+      console.log(paper.project.activeLayer);
     } else if (event.key === "enter") {
       closePath();
     } else if (event.key === "1") {
-      activateLayer("layer1", 1);
+      selectLayer('layer1');
     } else if (event.key === "2") {
-      activateLayer("layer2", 2);
+      selectLayer('layer2');
     } else if (event.key === "3") {
-      activateLayer("layer3", 3);
+      selectLayer('layer3');
     } else if (event.key === "4") {
-      activateLayer("layer4", 4);
+      selectLayer('layer4');
     } else if (event.key === "0") {
-      activateLayer("layerBg", 0);
+      selectLayer('layerBg');
     } else if (event.modifiers.shift && event.key === "n") {
       var clearAllConfirm = confirm("Are you sure you want to start new?");
 
       if (clearAllConfirm == true) {
-        var numOfLayers = document.querySelectorAll(".layers > div");
-        for (var i = 0; i < numOfLayers.length; i++) {
-          selectLayer(numOfLayers[i]);
-          clearLayer(app.activeLayerIndex);
-        }
-        selectLayer(numOfLayers[0]);
+        clearLayer('layer1');
+        clearLayer('layer2');
+        clearLayer('layer3');
+        clearLayer('layer4');
+        selectLayer('layer4');
       }
     } else if (event.modifiers.shift && event.key === "l") {
       var clearConfirm = confirm("Reset layer?");
       if (clearConfirm == true) {
-        clearLayer(app.activeLayerIndex);
+        clearLayer(paper.project.activeLayer.name);
       }
     } else if (event.modifiers.shift && event.key === "e") {
       canvasExport();
     } else if (event.key === "q") {
-      activateLayer("layer1", 1);
+      selectLayer('layer1');
       $('[data-remodal-id=modal]').remodal().open();
     } else if (event.key === "w") {
-      activateLayer("layer2", 2);
+      selectLayer('layer2');
       $('[data-remodal-id=modal]').remodal().open();
     } else if (event.key === "e") {
-      activateLayer("layer3", 3);
+      selectLayer('layer3');
       $('[data-remodal-id=modal]').remodal().open();
     } else if (event.key === "r") {
-      activateLayer("layer4", 4);
+      selectLayer('layer4');
       $('[data-remodal-id=modal]').remodal().open();
     } else if (event.key === "p") {
-      activateLayer("layerBg", 0);
+      selectLayer('layerBg');
       $('[data-remodal-id=modal]').remodal().open();
     } else if (event.key === "c") {
       app.tool = "free";
